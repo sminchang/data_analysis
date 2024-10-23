cf874c5668c6b52acec9511ab22143c5dc3a0f7e
Sung Minchang <58043537+sminchang@users.noreply.github.com>
2024-10-23 13:02:45 +0900

    Update HWP_as_PDF.py

--- HWP_as_PDF/HWP_as_PDF.py
+++ HWP_as_PDF/HWP_as_PDF.py
@@ -1,10 +1,12 @@
-# 윈도우와 한글 설치(아마 최신 버전)가 되어있어야 동작함
+# 윈도우와 한글(최신 버전) 설치가 되어있어야함
 # hwp 추출 후 pdf로 변환하는 파이썬 라이브러리들은 2차 가공을 거치기 때문에 신뢰하기 어려움
-# win32com은 윈도우가 한글을 직접 제어하여 pdf 변환 기능을 자동 수행함
-# win32com에서 제공하는 saveAs()함수로 간단하게 변환할 수 있는데, 모아찍기(2쪽씩) 옵션 해제가 안되는 문제가 있음
-# PrintToPDF 액션은 pdf 변환 옵션을 제어할 수 있어, 모아찍기 해제 후 변환할 수 있음 
+# win32com은 윈도우가 한글 프로그램을 직접 제어하여 pdf 변환 기능을 자동 수행함
+# win32com의 saveAs()함수로 간단하게 변환할 수 있는데, 모아찍기(2쪽씩) 옵션 해제가 안됨
+# PrintToPDF 액션은 pdf 변환 옵션을 제어할 수 있어, 모아찍기 해제 후 변환할 수 있어 사용
+# 테스트를 통해 누락되는 문서가 없는지 확인해가며 time.sleep()을 PC 성능에 맞게 조절해서 사용해야함
 
 import os
+import time
 import win32com.client #pip install pywin32
 
 def convert_hwp_to_pdf(hwp_path, pdf_path):
@@ -40,6 +42,8 @@ def convert_hwp_to_pdf(hwp_path, pdf_path):
                 
                 # PDF로 변환 실행
                 action.Execute(pSet)
+                #다음 작업까지 지연
+                time.sleep(0.5)
                 
             except Exception as e:
                 print(f'변환 실패: {src}. 오류: {str(e)}')
