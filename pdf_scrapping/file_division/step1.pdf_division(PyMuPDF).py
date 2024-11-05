12ecfff017578450e793e951558f544750a79a12
Sung Minchang <58043537+sminchang@users.noreply.github.com>
2024-11-05 14:54:58 +0900

    Update step1.pdf_division(PyMuPDF).py

--- pdf_scrapping/file_division/step1.pdf_division(PyMuPDF).py
+++ pdf_scrapping/file_division/step1.pdf_division(PyMuPDF).py
@@ -2,9 +2,8 @@
 # pypdf2는 내장 폰트 등의 문서 리소스를 파일 단위로 저장하기 때문에
 # 파일 크기 측면에서 pypdf2가 3~4배 정도 작은 파일 크기로 분할한다.
 
-# pymupdf는 pypdf2보다 다양한 pdf 구조를 인식해서 실행 중 오류가 거의 발생하지 않는다.
-
-# 파일 크기 및 작업 속도 측면에서는 pypdf2가, 안정성 측면에서는 pymupdf가 유리한 것으로 보인다.
+# pymupdf는 pypdf2보다 다양한 pdf 구조를 처리할 수 있다.
+# 파일 크기에서는 pypdf2가, 안정성 측면에서는 pymupdf가 유리한 것으로 보인다.
 
 import pdfplumber #pip install pdfplumber
 import fitz  # pip install PyMuPDF
