0f8a3dc17db56b2cdde9ecb6f70dd5b3f1a4289c
Sung Minchang <58043537+sminchang@users.noreply.github.com>
2025-01-09 13:46:54 +0900

    Update Request_HTML.py

--- web_scrapping/Request_HTML.py
+++ web_scrapping/Request_HTML.py
@@ -1,10 +1,14 @@
 import requests #pip install requests 
 from bs4 import BeautifulSoup #pip install beautifulsoup4
+import pandas as pd #pip install pandas, pip install openpyxl
 
 
 #URL 및 헤더 설정
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 base_url = 'https://www.codil.or.kr/viewSubTchStd.do?sType=type1All&sType2=&sortCase=ASC&pageIndex=' #뒤에 페이징 번호만 넣어주면 된다.
+
+#데이터 저장할 리스트 생성
+data = []
 
 #페이지 번호 순회(1~68)
 for page_num in range(1,69) :
@@ -24,8 +28,21 @@ for page_num in range(1,69) :
         #print(f"Original Text: {repr(place.text)}") #&nbsp;로 인한 문자열 형식 오류 확인
         infoType = tr.select_one('td:nth-child(4) > p:nth-child(2) > span')
 
-        print(title.text)
-        print(f'https://www.codil.or.kr/{title.get('href')}')
-        print(sourceInfo.text.replace('출처정보 : ', '').strip())
-        print(place.text.replace('발 행 처\xa0\xa0:','').strip())
-        print(infoType.text) #span id값 기반의 동적 요소로 추출되는 값이 없다. #infoType.get('id')로 span id값은 가져올 수 있다.
+        row_data = {
+
+            '제목': title.text,
+            '링크': f'https://www.codil.or.kr/{title.get('href')}',
+            '출처정보': sourceInfo.text.replace('출처정보 : ', '').strip(),
+            '발행처': place.text.replace('발 행 처\xa0\xa0:','').strip(),
+            '정보유형': infoType.text
+        }
+
+        # 데이터 리스트에 추가
+        data.append(row_data)
+
+
+# 데이터프레임 생성
+df = pd.DataFrame(data)
+
+# Excel 파일로 저장
+df.to_excel('codil_data.xlsx', index=False)
