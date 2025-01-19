43514184d2c4e68c1a135191aef63b5e7d9124c0
Sung Minchang <58043537+sminchang@users.noreply.github.com>
2025-01-19 16:20:56 +0900

    Update Request_Web_Driver.py

--- web_scrapping/Request_Web_Driver.py
+++ web_scrapping/Request_Web_Driver.py
@@ -10,10 +10,9 @@ from selenium.webdriver import ActionChains
 from selenium.webdriver.common.actions.wheel_input import ScrollOrigin
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager #pip install webdriver-manager
+
 import time
 import pandas as pd #pip install pandas, pip install openpyxl
-import re
-import os
 
 
 # 드라이버 설정
@@ -32,45 +31,39 @@ driver.get(url)
 data = []
 
 # 용어목록 갯수 확인
-rows = WebDriverWait(driver, 10).until(
-    EC.presence_of_all_elements_located((By.XPATH, '//*[@id="lelistwrapLeft"]/ul/li')) # DOM에 로딩될 때까지 대기(최대 10초 설정)
-)
+rows = WebDriverWait(driver,10).until(
+    EC.presence_of_all_elements_located((By.XPATH, '//*[@id="lelistwrapLeft"]/ul/li'))
+    )
 
 # 사이트 내 모든 페이지 순회
-for page_num in range(1, 2):
+for page_num in range(1, 1551):
     # 한 페이지 내 모든 목록 순회
-    for i in range(0, 12):#len(rows)+1):
+    for i in range(0, len(rows)+1):
         try:
             title_element = driver.find_element(By.XPATH, f'//*[@id="click{i}"]') # HTML 요소 추출
             title = title_element.get_attribute('title') # 태그 속성 추출
             title_element.click() #클릭 이벤트 적용
-        
-            dl_rows = WebDriverWait(driver, 3).until(
-            EC.presence_of_all_elements_located((By.XPATH, f'//*[@id="contentBody"]/dl')) # DOM에 로딩될 때까지 대기(최대 10초 설정)
+            
+            time.sleep(2)
+
+            # Ajax 로딩 완료를 기다리는 부분
+            dl_rows = WebDriverWait(driver, 10).until(
+                EC.presence_of_all_elements_located((By.XPATH, '//*[@id="contentBody"]/dl'))
             )
-            if dl_rows == 1:
-                content = WebDriverWait(driver, 3).until(
-                    EC.visibility_of_element_located((By.XPATH, f'//*[@id="contentBody"]/dl/dd[1]'))
-                ).text
-                ref_content = driver.find_element(By.XPATH, f'//*[@id="contentBody"]/dl/dd[2]').text
-                ref_link = driver.find_element(By.XPATH, f'//*[@id="contentBody"]/dl/dd[2]/a[1]').get_attribute('href')
 
-            else:
-                for content_num in range(1, len(dl_rows)+1):
-                    # #클릭 이벤트로 생성된 팝업창 로딩까지 대기
-                    content = WebDriverWait(driver, 3).until(
-                        EC.visibility_of_element_located((By.XPATH, f'//*[@id="contentBody"]/dl[{content_num}]/dd[1]'))
-                    ).text
-                    ref_content = driver.find_element(By.XPATH, f'//*[@id="contentBody"]/dl[{content_num}]/dd[2]').text
-                    ref_link = driver.find_element(By.XPATH, f'//*[@id="contentBody"]/dl[{content_num}]/dd[2]/a[1]').get_attribute('href')
-
-            row_data = {
-                "용어명": title,
-                "용어뜻": content,
-                '참조명': ref_content,
-                '참조링크': ref_link
-                }
-            data.append(row_data)
+            for content_num in range(1, len(dl_rows)+1):
+                print(title, len(dl_rows))
+                # #클릭 이벤트로 생성된 팝업창 로딩까지 대기
+                content = driver.find_element(By.XPATH,f'//*[@id="contentBody"]/dl[{content_num}]/dd[1]').text
+                ref_content = driver.find_element(By.XPATH, f'//*[@id="contentBody"]/dl[{content_num}]/dd[2]').text
+                ref_link = driver.find_element(By.XPATH, f'//*[@id="contentBody"]/dl[{content_num}]/dd[2]/a[1]').get_attribute('href')
+                row_data = {
+                    "용어명": title,
+                    "용어뜻": content,
+                    '참조명': ref_content,
+                    '참조링크': ref_link
+                    }
+                data.append(row_data)
 
         except Exception as e:
             print(f"Error : {e}")
