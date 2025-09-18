a44e758004e156f60ea313d0e2961169acf99c84
Sung Minchang <58043537+sminchang@users.noreply.github.com>
2025-09-18 14:59:49 +0900

    Update pdf_division.py

--- pdf_parsing/file_division_merge/pdf_division.py
+++ pdf_parsing/file_division_merge/pdf_division.py
@@ -1,7 +1,16 @@
 import os
 from PyPDF2 import PdfReader, PdfWriter
 
-# PyMuPDF가 속도측면에서 훨씬 빠르다. 다만 PyMuPDF는 원본의 내부 텍스트 레이어를 손상시켰는지, pdfplumber가 pdf 내용을 인식하지 못한다.
+# pymupdf는 내장 폰트 등의 문서 리소스를 페이지 단위로 저장하고
+# pypdf2는 내장 폰트 등의 문서 리소스를 파일 단위로 저장하기 때문에
+# 파일 크기 측면에서 pypdf2가 3~4배 정도 작은 파일 크기로 분할한다.
+# pymupdf는 pypdf2보다 다양한 pdf 구조를 처리할 수 있다.
+# pypdf2에서 안정성이 떨어지는 경우가 아니라면 pypdf2가 나아보인다.
+# pypdf2에서 폰트 및 기타 리소스 인식에 문제가 생길 경우 pymupdf로 해당 파일을 분할해볼 것을 추천한다.
+
+# PyMuPDF가 속도측면에서 훨씬 빠르다.
+# 다만 PyMuPDF는 원본의 내부 텍스트 레이어를 손상시켰는지, pdfplumber가 pdf 내용을 인식하지 못한다.
+# PyMuPDF의 라이센스는 내 코드를 오픈 소스로 공개하는 조건을 달고 있어, 꼭 필요한게 아니면 안쓰는게 낫다. 
 # 때문에 추후 데이터 추출을 고려한다면 PyPDF2를 사용한 분할작업이 낫다고 판단했다. 
 
 def split_pdf(input_pdf, output_dir, ranges):
@@ -81,4 +90,5 @@ if __name__ == "__main__":
     
     input_pdf = r"C:\Users\yunis\바탕 화면\test" # 실제 PDF 파일 이름으로 변경
     output_dir = r"C:\Users\yunis\바탕 화면\사업설명서_원본"  # 출력 디렉토리
+
     split_pdf(input_pdf, output_dir, ranges)
